6aa44dd39e88f4d95cc3c95492cca101410f178e
周颂杰 <mac@MacdeMacBook-Pro.local>
2023-11-21 15:42:56 +0800

    Committer: 周颂杰 <mac@MacdeMacBook-Pro.local>

---
+++ src/settings.js
@@ -0,0 +1,31 @@
+module.exports = {
+	/**
+	 * @type {string}
+	 * @description page title
+	 */
+	title: 'Blog Admin',
+
+	/**
+	 * @type {string}
+	 * @description logo URL
+	 */
+	logo: 'https://jay-markdown-image.oss-cn-guangzhou.aliyuncs.com/202311200046656.png',
+
+	/**
+	 * @type {boolean} true | false
+	 * @description Whether fix the header
+	 */
+	fixedHeader: true,
+
+	/**
+	 * @type {boolean} true | false
+	 * @description Whether show the logo in sidebar
+	 */
+	sidebarLogo: true,
+
+	/**
+	 * @type {Array}
+	 * @description 默认展开的父级菜单
+	 */
+	defaultOpeneds: ['/blog', '/page', '/pictureHosting', '/system', '/log', '/statistics']
+}
