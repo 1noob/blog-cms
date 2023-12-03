9a13388082358ffe4e00e16a257190ebc0f35e54
周颂杰 <mac@MacdeMacBook-Pro.local>
2023-12-03 20:33:57 +0800

    modified:   src/util/request.js

--- src/util/request.js
+++ src/util/request.js
@@ -4,7 +4,7 @@ import 'nprogress/nprogress.css'
 import {Message} from 'element-ui'
 
 const request = axios.create({
-	baseURL: 'https://blog-cms-vue.vercel.app/api/',
+	baseURL: 'https://vue.singsong.blog/api/',
 	timeout: 5000
 })
 
